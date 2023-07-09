c48e0b7b29c8f28395a7217d06d0bf95318caa19
dimitrov93 <ts.dimitrov93@gmail.com>
2023-07-09 18:43:39 +0300

    Frond-end initial skeleton

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+p {
+  font-size: 2rem;
+  
+}
+</style>
