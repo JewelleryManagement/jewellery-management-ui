6ee2e8b89bbc5187b498dd2bfd874a373a615fe1
Vladislav Katrankiev <33765600+VladoKat@users.noreply.github.com>
2023-07-17 15:27:19 +0300

    Feature 9/main/ Home window (#5)
    * chaning the port + readme
    
    * Update vue.config.js
    
    * Feature 9 home window (#4)
    
    * navbar
    
    * re-arranging navbar, router added
    
    * prittier to every file, route links changed, added css for nav__item active
    
    * Update App.vue
    
    ---------
    
    Co-authored-by: dimitrov93 <ts.dimitrov93@gmail.com>
    Co-authored-by: dimitrov93 <86495759+dimitrov93@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,117 @@
 <template>
-  <HelloWorld :msg="'Hello world!'"/>
+  <NavBar :pages="pages" />
+
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from "./components/Nav/NavBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,    
-}
-}
+    NavBar,
+  },
+  data() {
+    return {
+      pages: [
+        {
+          link: { text: "Home", url: "/" },
+        },
+        {
+          link: { text: "Users", url: "/users" },
+        },
+        {
+          link: { text: "Resources", url: "/resources" },
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
+:root {
+  --clr-white: #fff;
+  --clr-living-coral: #ff6f61;
+  --clr-inkwell: #363945;
+
+  --trans: all 400ms ease;
+
+  --container-width-lg: 75%;
+  --container-width-md: 86%;
+  --container-width-sm: 90%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* CSS Reset */
+
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default margin and padding */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ul,
+  ol,
+  li,
+  figure,
+  figcaption,
+  blockquote,
+  dl,
+  dd {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Remove list styles */
+  ul,
+  ol {
+    list-style: none;
+  }
+
+  /* Remove default hyperlink styles */
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  /* Remove outline on focus */
+  a:focus,
+  button:focus {
+    outline: none;
+  }
+
+  /* Remove default button styles */
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+
+  /* Remove table border spacing */
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+}
+
+.container {
+  max-width: var(--container-width-lg);
+  background-color: transparent;
+  margin: 0 auto;
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+  min-height: 100vh;
 }
 </style>
---
+++ src/views/Resources.vue
@@ -0,0 +1,25 @@
+<template>
+  <PageViewer :pageTitle="pageTitle" :content="content" />
+</template>
+
+<script>
+import PageViewer from "../components/PageViewer.vue";
+
+export default {
+  components: {
+    PageViewer,
+  },
+  data() {
+    return {
+      pageTitle: "Resources Page",
+      content: "This is the resources content",
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  text-align: center;
+}
+</style>
