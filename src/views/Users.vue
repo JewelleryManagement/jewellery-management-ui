be5343c38abd1ae410c9abe6761c780224b0a5bd
Aleksandur Dimitrov <52452769+aleks247@users.noreply.github.com>
2023-07-18 15:10:38 +0300

    Update Users.vue

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,19 +1,41 @@
 <template>
-  <PageViewer :pageTitle="pageTitle" :content="content" />
+  <user-list :users="users" />
 </template>
 <script>
 import PageViewer from "../components/PageViewer.vue";
+import UserList from "../components/UserList.vue";
 
 export default {
   components: {
+    UserList,
     PageViewer,
   },
   data() {
     return {
-      pageTitle: "Users Page",
-      content: "This is the users content",
-    };
-  },
+    users: [
+        {
+            name : 'Aleksandur Dimitrov',
+            email: 'aleksandurDimitrov@gmail.com',
+        },
+        {
+            name : 'Tsvetomir Dimitrov',
+            email: 'tsvetomirDimitrov@gmail.com',
+        },
+        {
+            name : 'Aleksander Pavlov',
+            email: 'aleksanderPavlov@gmail.com',
+        },
+        {
+            name : 'Vladislav Katrankiev',
+            email: 'vladislavKatrankiev@gmail.com',
+        },
+        {
+            name : 'Peter Katrankiev',
+            email: 'peterKatrankiev@gmail.com',
+        },
+      ],
+    }
+  }
 };
 </script>
 <style>
