71b611da39c2e5ae540d037daf7817246315e611
dimitrov93 <ts.dimitrov93@gmail.com>
2023-08-09 10:12:10 +0300

    complete nav bar

--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -14,14 +14,33 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn
-      v-for="(page, index) in pages"
-      :key="index"
-      :to="{ name: page.link.text }"
-      text
-    >
-      {{ page.link.text }}
-    </v-btn>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn
+        v-for="(page, index) in pages"
+        :key="index"
+        :to="{ name: page.link.text }"
+        text
+      >
+        {{ page.link.text }}
+      </v-btn>
+    </v-toolbar-items>
+
+    <v-menu v-if="smAndDown" >
+      <template v-slot:activator="{ props }">
+        <v-app-bar-nav-icon v-bind="props" size="x-large"></v-app-bar-nav-icon>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(page, index) in pages"
+          :key="index"
+          link
+          :to="{ name: page.link.text }"
+        >
+          {{ page.link.text }}
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
@@ -33,7 +52,16 @@ export default {
       this.$router.push({ name: pageName });
     },
   },
+  computed: {
+    smAndDown() {
+      return this.$vuetify.display.smAndDown
+    }
+  }
 };
 </script>
 
-<style></style>
+<style>
+.icon {
+  font-size: 50px;
+}
+</style>
