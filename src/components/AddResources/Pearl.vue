ef36e737f509dbc95d429c229c6b6d8ec41f1f6f
dimitrov93 <ts.dimitrov93@gmail.com>
2023-08-23 11:48:03 +0300

    created Pearl component

---
+++ src/components/AddResources/Pearl.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-text-field
+    v-model="type"
+    :counter="10"
+    :rules="nameRules"
+    label="Type"
+    required
+  ></v-text-field>
+
+  <v-text-field
+    v-model="size"
+    :counter="10"
+    :rules="nameRules"
+    label="Size"
+    required
+  ></v-text-field>
+
+  <v-text-field
+    v-model="quality"
+    :counter="10"
+    :rules="nameRules"
+    label="Quality"
+    required
+  ></v-text-field>
+
+  <v-text-field
+    v-model="color"
+    :counter="10"
+    :rules="nameRules"
+    label="Color"
+    required
+  ></v-text-field>
+
+  <v-text-field
+    v-model="shape"
+    :counter="10"
+    :rules="nameRules"
+    label="Shape"
+    required
+  ></v-text-field>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+
+  setup() {
+    const type = ref('');
+    const size = ref('');
+    const quality = ref('');
+    const color = ref('');
+    const shape = ref('');
+    const counter = ref('');
+
+    const nameRules = [
+      v => !!v || `Input field is required`,
+      v => (v && v.length <= 10) ? `At least 10 characters are required` : '',
+    ];
+    
+    return {type, size, quality, color, shape, nameRules, counter};
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,14 +1,7 @@
 <template>
   <v-container class="my-12" fluid>
     <v-sheet width="300" class="mx-auto">
-      <v-form ref="form">
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
-          required
-        ></v-text-field>
+      <v-form ref="formRef">
 
         <v-select
           v-model="select"
@@ -16,7 +9,11 @@
           :rules="[(v) => !!v || 'Item is required']"
           label="Item"
           required
+          @change="onItemChange($event)"
+          return-object
         ></v-select>
+
+        <Pearl v-if="select === 'Pearl'" />
 
         <v-checkbox
           v-model="checkbox"
@@ -30,7 +27,9 @@
             Submit
           </v-btn>
 
-          <v-btn color="error" class="mt-4" block @click="reset"> Reset </v-btn>
+          <v-btn color="error" class="mt-4" block @click="resetValidation">
+            Reset
+          </v-btn>
         </div>
       </v-form>
     </v-sheet>
@@ -38,10 +37,46 @@
 </template>
 
 <script>
+import { ref, onUpdated } from "vue";
+import Pearl from '../components/AddResources/Pearl.vue'
 export default {
+  components: {
+    Pearl
+  },
   setup() {
+    const formRef = ref(null);
+
+    const resetValidation = () => {
+      if (formRef.value) {
+        formRef.value.reset();
+        formRef.value.resetValidation();
+      }
+    };
+
     const items = ["Pearl", "PreciousMetal", "Gemstone", "LinkingPart"];
-    return { items };
+    const nameRules = ref("");
+    const validate = ref("");
+    const select = ref("");
+    const checkbox = ref(false);
+    const reset = ref("");
+    const rules = ref("");
+
+    const onItemChange = (e) => {
+      console.log(e);
+    }
+
+    return {
+      onItemChange,
+      formRef,
+      resetValidation,
+      nameRules,
+      checkbox,
+      select,
+      validate,
+      items,
+      reset,
+      rules,
+    };
   },
 };
 </script>
