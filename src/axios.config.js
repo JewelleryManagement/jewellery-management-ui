b215e591cef5c174bec891a0216dbd4fa5b61661
Vladislav Katrankiev <vladokatr@gmail.com>
2023-08-31 13:50:53 +0300

    [feature-ci-cd] make backend-url env specific

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://3.123.158.214:8080',
+    baseURL: process.env.VUE_APP_BASE_BACKEND_URL,
     timeout: 5000,
     headers: { 'Content-Type': 'application/json' },
 });
