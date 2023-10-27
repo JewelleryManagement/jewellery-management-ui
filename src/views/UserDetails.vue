437afa61ee98a0d0e6fec5ca0d2c29a8418dec8b
dimitrov93 <ts.dimitrov93@gmail.com>
2023-10-27 16:14:50 +0300

    adding table buttons to UserDetails

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -12,53 +12,79 @@
                   :resourcesAndQuantities="resourceItemResources"
                 ></user-card>
               </suspense>
-              <resource-availability-table
-                :tableColumns="tableColumnsResources"
-                :resourceItem="resourceItemResources"
-                :user="user"
-              ></resource-availability-table>
 
-              <v-card class="elevation-12 mt-4">
-                <div class="text-center">
-                  <h1>{{ user.name }}'s products table</h1>
-                </div>
-
-                <products-table
-                  :tableColumnsWithRCandPC="tableColumnsProducts"
-                  :userId="userId"
+              <div class="d-flex justify-center mt-10">
+                <v-btn
+                  color="red"
+                  @click="
+                    () => (isResourceTableVisible = !isResourceTableVisible)
+                  "
+                  >{{
+                    isResourceTableVisible ? "Hide Resources" : "Show Resources"
+                  }}</v-btn
                 >
-                  <template v-slot:item.resourceContent="{ item }">
-                    <v-icon @click="openDialog(item, 'resources')"
-                      >mdi-cube</v-icon
-                    >
-                  </template>
-
-                  <template v-slot:item.productsContent="{ item }">
-                    <v-icon @click="openDialog(item, 'products')"
-                      >mdi-cube-outline</v-icon
-                    >
-                  </template>
-
-                  <template v-slot:item.owner="{ item }">
-                    <v-icon>mdi-account-circle</v-icon>
-                  </template>
-                </products-table>
-
-                <resource-content-dialog
-                  v-if="isResourceDialogOpen"
-                  v-model="isResourceDialogOpen"
-                  :data="resourceDialogData"
-                  @close-dialog="closeDialog('resources')"
-                ></resource-content-dialog>
-
-                <products-content-dialog
-                  v-if="isProductsDialogOpen"
-                  v-model="isProductsDialogOpen"
-                  :data="productsDialogData"
-                  @close-dialog="closeDialog('products')"
+                <v-btn
+                  color="green"
+                  @click="
+                    () => (isProductsTableVisible = !isProductsTableVisible)
+                  "
+                  >{{
+                    isProductsTableVisible ? "Hide Products" : "Show Products"
+                  }}</v-btn
                 >
-                </products-content-dialog>
-              </v-card>
+              </div>
+
+              <transition>
+                <resource-availability-table
+                  v-if="isResourceTableVisible"
+                  :tableColumns="tableColumnsResources"
+                  :resourceItem="resourceItemResources"
+                  :user="user"
+                ></resource-availability-table>
+              </transition>
+
+              <transition>
+                <v-card class="elevation-12 mt-4" v-if="isProductsTableVisible">
+                  <div class="text-center">
+                    <h1>{{ user.name }}'s products table</h1>
+                  </div>
+
+                  <products-table
+                    :tableColumnsWithRCandPC="tableColumnsProducts"
+                    :userId="userId"
+                  >
+                    <template v-slot:item.resourceContent="{ item }">
+                      <v-icon @click="openDialog(item, 'resources')"
+                        >mdi-cube</v-icon
+                      >
+                    </template>
+
+                    <template v-slot:item.productsContent="{ item }">
+                      <v-icon @click="openDialog(item, 'products')"
+                        >mdi-cube-outline</v-icon
+                      >
+                    </template>
+
+                    <template v-slot:item.owner="{ item }">
+                      <v-icon>mdi-account-circle</v-icon>
+                    </template>
+                  </products-table>
+                </v-card>
+              </transition>
+              <resource-content-dialog
+                v-if="isResourceDialogOpen"
+                v-model="isResourceDialogOpen"
+                :data="resourceDialogData"
+                @close-dialog="closeDialog('resources')"
+              ></resource-content-dialog>
+
+              <products-content-dialog
+                v-if="isProductsDialogOpen"
+                v-model="isProductsDialogOpen"
+                :data="productsDialogData"
+                @close-dialog="closeDialog('products')"
+              >
+              </products-content-dialog>
             </v-col>
           </v-row>
         </v-container>
@@ -76,8 +102,16 @@ import { useStore } from "vuex";
 import ResourceAvailabilityTable from "@/components/Table/ResourceAvailabilityTable.vue";
 import ProductsTable from "@/components/Table/ProductsTable.vue";
 import UserCard from "@/components/Card/UserCard.vue";
-const [isResourceDialogOpen, resourceDialogData] = [ref(false), ref({})];
-const [isProductsDialogOpen, productsDialogData] = [ref(false), ref({})];
+const [isResourceDialogOpen, resourceDialogData, isResourceTableVisible] = [
+  ref(false),
+  ref({}),
+  ref(false),
+];
+const [isProductsDialogOpen, productsDialogData, isProductsTableVisible] = [
+  ref(false),
+  ref({}),
+  ref(false),
+];
 
 const { id } = defineProps(["id"]);
 const userId = id;
@@ -117,4 +151,14 @@ const closeDialog = (content) => {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
