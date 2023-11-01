eccfe424f12467ed5f2371fca2a3eacf38505a2f
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-01 09:50:01 +0200

    more refactoring on ProductsContentDialog

--- src/components/Dialog/ProductsContentDialog.vue
+++ src/components/Dialog/ProductsContentDialog.vue
@@ -8,19 +8,7 @@
     <template v-slot:default="{ isActive }">
       <v-card>
         <v-toolbar color="green" title="Products Content..."></v-toolbar>
-        <products-table
-          :products="productsContent"
-        >
-          <template v-slot:item.resourceContent="{ item }">
-            <v-icon @click="openDialog(item, 'resources')">mdi-cube</v-icon>
-          </template>
-
-          <template v-slot:item.productsContent="{ item }">
-            <v-icon @click="openDialog(item, 'products')"
-              >mdi-cube-outline</v-icon
-            >
-          </template>
-
+        <products-table :products="productsContent">
           <template v-slot:item.owner="{ item }">
             <router-link
               style="text-decoration: none; color: inherit"
@@ -45,34 +33,16 @@
       </v-card>
     </template>
   </v-dialog>
-
-  <resource-content-dialog
-    v-if="isResourceDialogOpen"
-    v-model="isResourceDialogOpen"
-    :data="resourceDialogData"
-    @close-dialog="closeDialog('resources')"
-  ></resource-content-dialog>
-
-  <products-content-dialog
-    v-if="isProductsDialogOpen"
-    v-model="isProductsDialogOpen"
-    :data="productsDialogData"
-    @close-dialog="closeDialog('products')"
-  >
-  </products-content-dialog>
 </template>
 
 <script setup>
-import { useStore } from "vuex";
-import { ref, computed } from "vue";
-const store = useStore();
+import { ref } from "vue";
 import ProductsTable from "../Table/ProductsTable.vue";
 const { modelValue, data } = defineProps({
   modelValue: Boolean,
   data: Object,
 });
-const [isResourceDialogOpen, resourceDialogData] = [ref(false), ref({})];
-const [isProductsDialogOpen, productsDialogData] = [ref(false), ref({})];
+
 const emits = defineEmits(["close-dialog"]);
 
 const productsContent = ref(data.productsContent);
@@ -80,22 +50,4 @@ productsContent.value = productsContent.value.map((product) => ({
   ...product,
   authors: product.authors.map((a) => a.name).join(", "),
 }));
-
-const openDialog = (item, content) => {
-  if (content == "resources") {
-    resourceDialogData.value = item;
-    isResourceDialogOpen.value = true;
-  } else {
-    productsDialogData.value = item;
-    isProductsDialogOpen.value = true;
-  }
-};
-
-const closeDialog = (content) => {
-  if (content === "resources") {
-    isResourceDialogOpen.value = false;
-  } else {
-    isProductsDialogOpen.value = false;
-  }
-};
 </script>
