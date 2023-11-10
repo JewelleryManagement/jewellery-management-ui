06b00d937fa3f1f69e78cb571fb4ce34a6fb4ddd
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-10 22:41:19 +0200

    transfer button updated with transfer dialog

--- src/components/Button/ProductTransferButton.vue
+++ src/components/Button/ProductTransferButton.vue
@@ -11,86 +11,31 @@
     <v-icon color="#607D8B" size="25">mdi-swap-horizontal</v-icon>
   </v-btn>
 
-  <v-dialog v-model="dialog">
-    <v-form @submit.prevent="handleSubmit" ref="form" class="mt-4">
-      <v-card class="mx-auto text-center pa-2" width="400">
-        <v-card-title>
-          Catalog Number: {{ item.catalogNumber }}
-        </v-card-title>
-        <v-select
-          :items="userOptions"
-          v-model="user"
-          label="Select User"
-          required
-          :rules="[validateUser]"
-        ></v-select>
-        <div class="d-flex mx-auto">
-          <v-btn color="success" class="mt-4" type="submit">Submit</v-btn>
-          <v-btn color="error" class="mt-4" @click="closeDialog">Close</v-btn>
-        </div>
-      </v-card>
-    </v-form>
-  </v-dialog>
+  <product-transfer-dialog
+    v-if="isResourceDialogOpen"
+    v-model="isResourceDialogOpen"
+    :item="item"
+    :userId="userId"
+    @close-dialog="closeDialog"
+  />
 </template>
 
 <script setup>
-import { validateUser } from "@/utils/validation-rules";
-import { ref, computed, inject } from "vue";
-import { useStore } from "vuex";
+import ProductTransferDialog from "../Dialog/ProductTransferDialog.vue";
+import { ref } from "vue";
+
 const props = defineProps({
   item: Object,
   userId: { type: String, default: null },
 });
-const snackbarProvider = inject("snackbarProvider");
 const [item, userId] = [props.item, props.userId];
-const store = useStore();
-const allUsers = computed(() => store.getters["users/allUsers"]);
-const userOptions = allUsers.value.map((user) => user.name);
-const [user, form, dialog] = [ref(""), ref(null), ref(false)];
-
-async function fetchProducts() {
-  try {
-    store.dispatch("products/fetchProducts");
-  } catch (error) {
-    snackbarProvider.showErrorSnackbar("Failed to fetch products.");
-  }
-}
-
-async function fetchProductsForUser() {
-  try {
-    await store.dispatch("products/fetchProductsByOwner", userId);
-  } catch (error) {
-    snackbarProvider.showErrorSnackbar("Failed to fetch user products.");
-  }
-}
-
-const handleSubmit = async () => {
-  const { valid } = await form.value.validate();
-  const selectedUser = user.value;
-
-  if (!valid) return;
-
-  const data = {
-    productId: item.id,
-    recipientId: allUsers.value.find((user) => user.name == selectedUser).id,
-  };
-
-  try {
-    await store.dispatch("products/transferProduct", data);
-    userId ? await fetchProductsForUser() : await fetchProducts();
-    snackbarProvider.showSuccessSnackbar("Successfully transferred product!");
-    closeDialog();
-  } catch (error) {
-    snackbarProvider.showErrorSnackbar("Couldn't transfer the product!");
-  }
-};
+const isResourceDialogOpen = ref(false);
 
 const openDialog = () => {
-  dialog.value = true;
+  isResourceDialogOpen.value = true;
 };
 
 const closeDialog = () => {
-  form.value.reset()
-  dialog.value = false;
+  isResourceDialogOpen.value = false;
 };
 </script>
---
+++ src/components/Dialog/ProductTransferDialog.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-dialog :model-value="modelValue" @update:model-value="modelValue">
+    <v-form @submit.prevent="handleSubmit" ref="form" class="mt-4">
+      <v-card class="mx-auto text-center pa-2" max-width="400" min-width="200">
+        <v-card-title> Catalog Number: {{ item.catalogNumber }} </v-card-title>
+        <v-select
+          :items="userOptions"
+          v-model="user"
+          label="Select User"
+          required
+          :rules="[validateUser]"
+        ></v-select>
+        <div class="d-flex mx-auto">
+          <v-btn color="success" class="mt-4" type="submit">Submit</v-btn>
+          <v-btn color="error" class="mt-4" @click="closeDialog">Close</v-btn>
+        </div>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
+
+<script setup>
+import { validateUser } from "@/utils/validation-rules";
+import { ref, computed, inject } from "vue";
+import { useStore } from "vuex";
+const snackbarProvider = inject("snackbarProvider");
+const props = defineProps(["modelValue", "item", "userId"]);
+const { modelValue, item, userId } = props;
+const [user, form] = [ref(""), ref(null)];
+const store = useStore();
+
+const emits = defineEmits(["close-dialog"]);
+
+const allUsers = computed(() => store.getters["users/allUsers"]);
+const userOptions = allUsers.value.map((user) => user.name);
+
+async function fetchProducts() {
+  try {
+    store.dispatch("products/fetchProducts");
+  } catch (error) {
+    snackbarProvider.showErrorSnackbar("Failed to fetch products.");
+  }
+}
+
+async function fetchProductsForUser() {
+  try {
+    await store.dispatch("products/fetchProductsByOwner", userId);
+  } catch (error) {
+    snackbarProvider.showErrorSnackbar("Failed to fetch user products.");
+  }
+}
+
+const handleSubmit = async () => {
+  const { valid } = await form.value.validate();
+  const selectedUser = user.value;
+
+  if (!valid) return;
+
+  const data = {
+    productId: item.id,
+    recipientId: allUsers.value.find((user) => user.name == selectedUser).id,
+  };
+  try {
+    await store.dispatch("products/transferProduct", data);
+    userId ? await fetchProductsForUser() : await fetchProducts();
+    snackbarProvider.showSuccessSnackbar("Successfully transferred product!");
+    closeDialog();
+  } catch (error) {
+    snackbarProvider.showErrorSnackbar("Couldn't transfer the product!");
+  }
+};
+
+const closeDialog = async () => {
+  resetForm();
+  emits("close-dialog");
+};
+
+const resetForm = () => {
+  if (form.value) {
+    form.value.reset();
+    form.value.resetValidation();
+  }
+};
+</script>
