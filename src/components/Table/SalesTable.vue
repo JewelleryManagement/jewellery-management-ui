4bc2774fcac50556da916224f9fd113a2968ba55
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-14 09:34:30 +0200

    created sales table

---
+++ src/components/Table/SalesTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="elevation-12">
+    <div class="text-center">
+      <h1>Sales table</h1>
+    </div>
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="tableColumns" :items="products" :search="search">
+      <template v-for="(_, slot) in $slots" v-slot:[slot]="scope">
+        <slot :name="slot" v-bind="scope || {}" />
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script setup>
+import { VDataTable } from "vuetify/labs/VDataTable";
+import { ref, computed } from "vue";
+const search = ref("");
+
+const tableColumns = [
+  { key: "productName", title: "Product Name" },
+  { key: "price", title: "Price" },
+  { key: "discount", title: "Discount" },
+];
+
+const products = [
+  { productName: "Product 1", price: 10, discount: 0 },
+  { productName: "Product 2", price: 15, discount: 5 },
+];
+</script>
--- src/views/Sales.vue
+++ src/views/Sales.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
-        Sales
-    </div>
+  <v-container class="my-12" fluid>
+    <v-row justify="center">
+      <v-col cols="10" max-width="1600">
+        <sales-table></sales-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-
+import SalesTable from "@/components/Table/SalesTable.vue";
 </script>
