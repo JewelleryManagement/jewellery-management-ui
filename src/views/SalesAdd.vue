ec0623c1b7fe1f72f19a03f83a57b962cfa0005c
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-15 23:36:37 +0200

    adjusted some functions and variables

--- src/views/SalesAdd.vue
+++ src/views/SalesAdd.vue
@@ -4,54 +4,77 @@
       <div class="mx-auto text-center" style="font-size: 24px">
         {{ pageTitle }}
       </div>
-      <v-form @submit.prevent="handleSubmit">
-        <v-select label="Seller" v-model="seller" :items="allUsersNames">
+      <v-form @submit.prevent="handleSubmit" ref="form">
+        <v-select
+          label="Seller"
+          v-model="seller"
+          :items="allUsersNames"
+          :rules="[() => validateAuthors(seller)]"
+          lazy-rules
+        >
         </v-select>
 
         <v-select
           v-model="buyer"
           label="Buyer"
           :items="allUsersNames"
-          :rules="[validateAuthors(buyer)]"
+          :rules="[() => validateAuthors(buyer)]"
+          lazy-rules
         >
         </v-select>
 
-        <v-btn color="primary" @click="openDialog" :disabled="!selectedUser">
+        <v-btn
+          color="primary"
+          @click="toggleDialog(true)"
+          block
+          :disabled="!selectedUser"
+        >
           Products
         </v-btn>
 
-        <div v-if="productsContent.length > 0">
-          Currently selected products:
-          <span v-for="(product, i) in productsContent" :key="product">
-            <v-row class="mt-4">
-              <v-col cols="8" class="pa-0">
-                <v-text-field
-                  prefix="€"
-                  label="Price"
-                  v-model="productsContent[i].salePrice"
-                ></v-text-field>
-              </v-col>
-              <v-divider vertical style="height: 3.5rem"></v-divider>
+        <v-container v-if="productsContent.length > 0">
+          <div class="mx-auto text-center" style="font-size: 16px">
+            Currently selected products:
+          </div>
+          <v-row
+            class="mt-4"
+            v-for="(product, i) in productsContent"
+            :key="product"
+          >
+            <v-col cols="8" class="pa-0">
+              <v-text-field
+                prefix="€"
+                label="Price"
+                v-model="productsContent[i].salePrice"
+              ></v-text-field>
+            </v-col>
+            <v-divider vertical style="height: 3.5rem"></v-divider>
 
-              <v-col cols="4" class="pa-0">
-                <v-text-field
-                  suffix="%"
-                  label="Discount"
-                  v-model="productsContent[i].discount"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </span>
-        </div>
+            <v-col cols="4" class="pa-0">
+              <v-text-field
+                suffix="%"
+                label="Discount"
+                v-model="productsContent[i].discount"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
 
-        <div class="d-flex flex-column mt-4">
-          <p>Total amount: {{ totalAmount || 0 }}</p>
-          <p>Discounted amount: {{ discountedAmount || 0 }}</p>
+        <v-container class="d-flex flex-column mt-4">
+          <p>Total amount: € {{ totalAmount || 0 }}</p>
+          <p>Discounted amount: € {{ discountedAmount || 0 }}</p>
           <p>Total discount: {{ totalDiscount.toFixed(2) || 0 }} %</p>
-        </div>
+        </v-container>
 
         <div class="d-flex flex-column">
-          <v-btn color="success" class="mt-4" block type="submit">Submit</v-btn>
+          <v-btn
+            color="success"
+            class="mt-4"
+            block
+            type="submit"
+            :disabled="!formValid"
+            >Submit</v-btn
+          >
           <v-btn color="error" class="mt-4" block>Reset</v-btn>
           <v-btn color="warning" class="mt-4" block @click="router.go(-1)"
             >Go Back</v-btn
@@ -62,7 +85,7 @@
     <products-dialog
       v-if="selectedUser"
       v-model="productsDialog"
-      @close-dialog="closeDialog"
+      @close-dialog="toggleDialog(false)"
       @save-product-dialog="productsTableValues"
       :userId="selectedUser.id"
     >
@@ -75,20 +98,33 @@ import { validateAuthors } from "../utils/validation-rules";
 import ProductsDialog from "@/components/Dialog/ProductsDialog.vue";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
-import { ref, computed, watch } from "vue";
-const route = useRoute();
-const router = useRouter();
-const seller = ref([]);
-
+import { ref, computed, watch, watchEffect } from "vue";
+const [route, router] = [useRoute(), useRouter()];
+const store = useStore();
+const pageTitle = ref(route.meta.title);
+const [seller, buyer] = [ref([]), ref([])];
+const [form, formValid] = [ref(null), ref(true)];
 const selectedUser = ref("");
+const [productsDialog, productsContent] = [ref(false), ref([])];
+
+const allUsers = computed(() => store.getters["users/allUsers"]);
+const allUsersNames = allUsers.value.map((user) => user.name);
+
+// Watches the form value and disables submit button
+watchEffect(() => {
+  if (form.value) {
+    form.value.validate().then(({ valid }) => {
+      formValid.value = valid;
+    });
+  }
+});
+
+// Watches for changes in seller input in order to trigger product's dialog for the seller only
 watch(seller, (newValue) => {
   selectedUser.value = allUsers.value.find((user) => user.name == seller.value);
   productsContent.value = [];
 });
 
-const buyer = ref([]);
-const store = useStore();
-const pageTitle = ref(route.meta.title);
 const totalAmount = computed(() =>
   productsContent.value.reduce((acc, cur) => acc + Number(cur.salePrice), 0)
 );
@@ -112,28 +148,19 @@ const totalDiscount = computed(() => {
     return 0;
   }
 
-  return 100 - ((discountedAmountValue / totalAmountValue) * 100);
+  return 100 - (discountedAmountValue / totalAmountValue) * 100;
 });
 
-const [productsDialog, productsContent] = [ref(false), ref([])];
-
-const allUsers = computed(() => store.getters["users/allUsers"]);
-const allUsersNames = allUsers.value.map((user) => user.name);
-
-const openDialog = () => {
-  productsDialog.value = true;
-};
-
-const closeDialog = () => {
-  productsDialog.value = false;
+const toggleDialog = (isOpen) => {
+  productsDialog.value = isOpen;
 };
 
 const productsTableValues = (productsContentValue) => {
   productsContent.value = productsContentValue;
-  closeDialog("products");
+  toggleDialog(false);
 };
 
-const handleSubmit = () => {
+const handleSubmit = async () => {
   console.log(productsContent.value);
 };
 </script>
