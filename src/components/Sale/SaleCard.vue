9e3ed374d1b18f22731b4de04ab5c4430f5d3633
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-28 17:54:29 +0200

    SaleParticipantInfo updated

--- src/components/Sale/SaleCard.vue
+++ src/components/Sale/SaleCard.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card class="mx-auto mt-10 text-center elevation-2" max-width="600">
     <div class="d-flex flex-sm-row flex-column justify-space-around mt-4">
-      <SaleParticipantsInfo title="Seller" :participant="currentSale.seller" />
-      <SaleParticipantsInfo title="Buyer" :participant="currentSale.buyer" />
+      <SaleParticipantInfo title="Seller" :participant="currentSale.seller" />
+      <SaleParticipantInfo title="Buyer" :participant="currentSale.buyer" />
     </div>
 
     <br />
@@ -12,7 +12,7 @@
 </template>
 
 <script setup>
-import SaleParticipantsInfo from "./SaleParticipantInfo.vue";
+import SaleParticipantInfo from "./SaleParticipantInfo.vue";
 import SaleInformation from "./SaleInformation.vue";
 const { currentSale } = defineProps({ currentSale: Object });
 </script>
