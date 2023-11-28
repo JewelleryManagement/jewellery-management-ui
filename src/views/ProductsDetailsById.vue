78b0e6721b079d2a7386b94ab7fedd662ab78468
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-28 22:56:53 +0200

    responsive added

--- src/views/ProductsDetailsById.vue
+++ src/views/ProductsDetailsById.vue
@@ -1,70 +1,63 @@
 <template>
   <v-container>
     <v-card
-      class="d-flex my-14 mx-auto"
+      class="d-flex flex-lg-row flex-md-row flex-column justify-space-between mx-auto mt-10"
       elevation="8"
       rounded="lg"
       max-width="1000"
     >
-      <v-container class="pa-0">
-        <v-img
-          :lazy-src="defaultPicture"
-          :src="picture ?? defaultPicture"
-          cover
-          width="100%"
-          height="100%"
-        >
-        </v-img>
-      </v-container>
+      <v-img
+        :lazy-src="defaultPicture"
+        :src="picture ?? defaultPicture"
+        cover
+        :max-width="isMediumScreen() ? '100%': '60%'" 
+      >
+      </v-img>
 
-      <v-container>
-        <div class="d-flex flex-column align-center">
-          <div>
-            <strong>Catalog Number:</strong>
-            {{ currentProductInfo.catalogNumber }}
-          </div>
-          <div>
-            <strong>Description:</strong> {{ currentProductInfo.description }}
-          </div>
-          <div class="d-flex">
-            <strong>Owner:&nbsp;</strong
-            ><user-tool-tip :user="currentProductInfo.owner" />
-          </div>
-          <div>
-            <strong>Authors:&nbsp;</strong>
-            <user-tool-tip
-              :user="author"
-              v-for="(author, index) in currentProductInfo.authors"
-              :key="author"
-              @click.stop
-            >
-              <template v-if="index < currentProductInfo.authors.length - 1"
-                >&comma;&nbsp;</template
-              >
-            </user-tool-tip>
-          </div>
-          <div>
-            <strong>Content of:</strong> {{ currentProductInfo.contentOf }}
-          </div>
-
-          <strong>Part of Sale: </strong>
-          <PartOfsale :partOfSale="currentProductInfo.partOfSale"
-            >N/A</PartOfsale
-          >
-          <bar-code :productionNumber="currentProductInfo.productionNumber" />
-
-          <picture-button @picture-selected="handlePictureSelected" />
-
-          <div class="d-flex justify-center mt-10">
-            <v-btn color="red" @click="openDialog('resources')" @click.stop
-              >Resources</v-btn
-            >
-            <v-btn color="green" @click="openDialog('products')" @click.stop
-              >Products</v-btn
-            >
-          </div>
+      <div class="d-flex flex-column align-center my-4 w-auto">
+        <div>
+          <strong>Catalog Number:</strong>
+          {{ currentProductInfo.catalogNumber }}
         </div>
-      </v-container>
+        <div>
+          <strong>Description:</strong> {{ currentProductInfo.description }}
+        </div>
+        <div class="d-flex">
+          <strong>Owner:&nbsp;</strong
+          ><user-tool-tip :user="currentProductInfo.owner" />
+        </div>
+        <div>
+          <strong>Authors:&nbsp;</strong>
+          <user-tool-tip
+            :user="author"
+            v-for="(author, index) in currentProductInfo.authors"
+            :key="author"
+            @click.stop
+          >
+            <template v-if="index < currentProductInfo.authors.length - 1"
+              >&comma;&nbsp;</template
+            >
+          </user-tool-tip>
+        </div>
+        <div>
+          <strong>Content of:</strong> {{ currentProductInfo.contentOf }}
+        </div>
+
+        <strong>Part of Sale: </strong>
+        <PartOfsale :partOfSale="currentProductInfo.partOfSale">N/A</PartOfsale>
+        <bar-code :productionNumber="currentProductInfo.productionNumber" />
+
+        <picture-button @picture-selected="handlePictureSelected" />
+
+        <div class="d-flex justify-center mt-10">
+          <v-btn color="red" @click="openDialog('resources')" @click.stop
+            >Resources</v-btn
+          >
+          <v-btn color="green" @click="openDialog('products')" @click.stop
+            >Products</v-btn
+          >
+        </div>
+      </div>
     </v-card>
 
     <resource-content-dialog
@@ -85,6 +78,7 @@
 </template>
 
 <script setup>
+import { isMediumScreen } from "@/utils/display";
 import { ref, computed, inject } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
@@ -92,7 +86,7 @@ import PartOfsale from "@/components/Tooltip/PartOfsale.vue";
 const snackbarProvider = inject("snackbarProvider");
 const isResourceDialogOpen = ref(false);
 const isProductsDialogOpen = ref(false);
-const defaultPicture = require("../assets/logo.png");
+const defaultPicture = require("../assets/no-pic.png");
 const store = useStore();
 const route = useRoute();
 const router = useRouter();
