cb4a6ea8321a133fd3e93b4eb0482d40464c8a7d
dimitrov93 <ts.dimitrov93@gmail.com>
2023-12-20 17:27:21 +0200

    fix for the routing after a product return

--- src/components/Button/ReturnProductButton.vue
+++ src/components/Button/ReturnProductButton.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-btn
-      v-if="!isSalesPage && isPartOfProduct"
-      color="#673AB7"
-      @click.stop="submitReturn"
-      :size="isMediumScreen() ? 'x-small' : 'default'"
-      >Product Return</v-btn
-    >
+  <v-btn
+    v-if="!isSalesPage && isPartOfProduct"
+    color="#673AB7"
+    @click.stop="submitReturn"
+    :size="isMediumScreen() ? 'x-small' : 'default'"
+    >Product Return</v-btn
+  >
   <v-btn variant="plain" @click.stop="submitReturn" v-else-if="isSalesPage">
     <v-icon size="25">mdi-cart-remove</v-icon>
   </v-btn>
@@ -19,11 +19,10 @@ import { inject } from "vue";
 const store = useStore();
 const [router, route] = [useRouter(), useRoute()];
 const snackbarProvider = inject("snackbarProvider");
-const isSalesPage = route.path.includes("/sales")
-const isPartOfProduct = props.currentProductInfo.partOfSale
+const isSalesPage = route.path.includes("/sales");
+const isPartOfProduct = props.currentProductInfo.partOfSale;
 const props = defineProps({ currentProductInfo: Object });
 const currentProductId = props.currentProductInfo.id;
-
 const submitReturn = () => {
   const confirm = window.confirm(
     "Are you sure that you would like to return this product?"
@@ -34,11 +33,11 @@ const submitReturn = () => {
   submitSaleReturn();
 };
 
-const submitSaleReturn = () => {
+const submitSaleReturn = async () => {
   try {
-    store.dispatch("sales/returnProduct", currentProductId);
+    await store.dispatch("sales/returnProduct", currentProductId);
     snackbarProvider.showSuccessSnackbar("Product has been returned from sale");
-    router.go(-1);
+    isSalesPage ? router.push('/sales') : router.push('/products')
   } catch (error) {
     snackbarProvider.showErrorSnackbar(error?.response?.data?.error);
   }
