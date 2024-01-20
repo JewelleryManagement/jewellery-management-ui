7489822c90d31d8d8c1c52e0ed83dac358e9f3c5
Vladislav Katrankiev <vladokatr@gmail.com>
2024-01-20 19:42:55 +0200

    fix: remove server startup from playwright config. use baseUrl for tests

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Login page landing', async ({ page }) => {
-  await page.goto('localhost:3000');
+  await page.goto('./');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Login page/);
 });
 
 test('Login successfully', async ({ page }) => {
-  await page.goto('localhost:3000');
+  await page.goto('./');
 
   await page.getByPlaceholder("Email Address").fill("root@gmail.com")
   await page.getByPlaceholder("Password").fill("p@s5W07d")
