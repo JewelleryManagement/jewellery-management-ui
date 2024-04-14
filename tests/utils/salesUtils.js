2fccaacf892c99a312b5706ca92564e649f96f3c
dimitrov93 <ts.dimitrov93@gmail.com>
2024-04-15 00:27:22 +0300

    [test-fix-4]

--- tests/utils/salesUtils.js
+++ tests/utils/salesUtils.js
@@ -21,7 +21,7 @@ export const firstInputSelect = async (page) => {
 
 export const secondInputSelect = async (page) => {
   await page.locator(".v-field__input").nth(1).click();
-  await page.getByRole("option", { name: "testUser1 test testUser1@gmail.com", exact: true }).click();
+  await page.getByRole("option", { name: "admin test admin@gmail.com", exact: true }).click();
 };
 
 export const selectDate = async (page, expect) => {
